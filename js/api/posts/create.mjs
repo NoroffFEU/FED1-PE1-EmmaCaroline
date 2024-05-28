64f4657e8bcf671dcacf29b166a2d9b6ba9db859
EmmaCaroline <emma-caroline@hotmail.com>
2024-05-28 03:45:31 +0200

    Change some of the code after html validation

--- js/api/posts/create.mjs
+++ js/api/posts/create.mjs
@@ -1,37 +1,6 @@
 
 import { createBlogPost } from '../posts/apiCalls.mjs';
 
-/*document.addEventListener('DOMContentLoaded', () => {
-    const createPostForm = document.getElementById('createPostForm');
-
-    createPostForm.addEventListener('submit', async (event) => {
-        event.preventDefault();
-        const formData = new FormData(createPostForm);
-        const post = Object.fromEntries(formData.entries());
-
-        // Handle the media field as optional
-        if (post.media) {
-            post.media = {
-                url: post.media,
-                alt: post.title // Assuming you want to use the title as alt text
-            };
-        } else {
-            delete post.media;
-        }
-
-        const response = await createBlogPost(post); // Use createBlogPost function
-
-        if (response.ok) {
-            alert('Post created successfully!');
-            createPostForm.reset();
-        } else {
-            alert('Failed to create post.');
-        }
-    });
-});*/
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const createPostForm = document.getElementById('createPostForm');
 
