3e82f654234fecdcc60fa1be4a0b90d223fa6e3d
EmmaCaroline <emma-caroline@hotmail.com>
2024-05-28 19:10:08 +0200

    fixed path

--- js/api/auth/logoff.mjs
+++ js/api/auth/logoff.mjs
@@ -3,5 +3,5 @@ import { clearToken } from "./authFetch.mjs";
 
 export function logOff() {
     clearToken();
-    window.location.href = "/FED1-PE1-EMMACAROLINE/index.html";
+    window.location.href = "/FED1-PE1-EmmaCaroline/index.html";
 }
