e9d01703cfe23dcb6ffb15a442550e95e1b8cc78
EmmaCaroline <emma-caroline@hotmail.com>
2024-05-28 17:11:12 +0200

    Added alert to subscribe button

--- js/homepage/clearButton.mjs
+++ js/homepage/clearButton.mjs
@@ -1,6 +1,12 @@
 
-// Clears the textarea for the subscribe button, to improve the realism of the site
+// Clears the textarea and sends an alert for the subscribe button, to improve the realism of the site
 
 document.getElementById('clearButton').addEventListener('click', function() {
-    document.getElementById('subscribe-email').value = '';
-  });
+  const emailInput = document.getElementById('subscribe-email');
+  const email = emailInput.value.trim(); // Remove leading and trailing whitespaces
+
+  if (email) {
+      emailInput.value = '';
+      alert("You have subscribed to our newsletter. Check your email.");
+  }
+});
